0bef8b34c9571a91190606692d1c034396060e01
tsai810417 <tsai_cheng@yahoo.com>
2018-07-25 16:50:15 +0800

    try v-if attr

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ var app = new Vue({
   el: "#app", // specify where the Vue object will live
   data: {
     message: 'Hello Vue!',
+    seen: true,
   },
 });
+
+
+
+// can change the value of the message key
+// app.message = 'data has been changed';
+
+// seen = false, the span will not be displayed
+// app.seen = false;
